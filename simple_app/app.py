459a9e1d8474d4305ce5da04a2b4e77e4ca9b0e9
peace <jose.s.contacto@gmail.com>
2024-02-12 15:30:06 +0000

    tdd: Add basic app
    Se genera un hook en local de pre commit, que sera versionada en el
    siguiente commit

--- simple_app/app.py
+++ simple_app/app.py
@@ -1,9 +1,9 @@
+"""Containing app class"""
 class App:
     """App class"""
+
     def say_hello(self) -> str:
         """
         Let's be polite, and say hello.
         """
         return "hello!"
-
-
--- simple_app/tests/test_hello.py
+++ simple_app/tests/test_hello.py
@@ -1,5 +1,7 @@
+"""App unitesting for tdd"""
 from ..app import App
 
 def test_system_should_say_hello():
+    """System should say hello to user"""
     app = App()
     assert app.say_hello() == "hello!"
