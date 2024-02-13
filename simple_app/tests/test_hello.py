a29be859e3dc72cfba470ba02309138148813521
peace <jose.s.contacto@gmail.com>
2024-02-13 12:11:11 +0000

    Add pytest integration mark

--- simple_app/tests/test_hello.py
+++ simple_app/tests/test_hello.py
@@ -1,7 +1,12 @@
 """App unitesting for tdd"""
 from ..app import App
+import pytest
 
 def test_system_should_say_hello():
     """System should say hello to user"""
     app = App()
     assert app.say_hello() == "hello!"
+
+@pytest.mark.integration
+def test_integration_test():
+    assert True
