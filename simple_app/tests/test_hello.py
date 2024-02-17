6943fe4cee801fb95fd6a9bf78285b569dfbcc94
peaceprogress <159674459+peaceprogress@users.noreply.github.com>
2024-02-17 17:14:22 +0000

    Update test_hello.py

--- simple_app/tests/test_hello.py
+++ simple_app/tests/test_hello.py
@@ -6,6 +6,7 @@ def test_system_should_say_hello():
     """System should say hello to user"""
     app = App()
     assert app.say_hello() == "hello!"
+    assert False
 
 @pytest.mark.integration
 def test_integration_test():
